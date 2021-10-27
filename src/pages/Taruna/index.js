f29fd3962e2946f0881a62ecfce5505095ec94d1
Sturgen <dodi.alexsander@gmail.com>
2021-10-28 06:41:47 +0700

    tr,youth,nextgen

--- src/pages/Taruna/index.js
+++ src/pages/Taruna/index.js
@@ -6,21 +6,23 @@ import { colors } from '../../random';
 class Taruna extends Component {
     render() {
         return (
-            <Container style={container} fluid>
-                <div>
-                    <FotoFoto
-                        foto1='https://live.staticflickr.com/65535/51633040975_1455138117_z.jpg'
-                        foto2='https://live.staticflickr.com/65535/51631364697_a962098b27_z.jpg'
-                        foto3='https://live.staticflickr.com/65535/51632837819_e6810e9a95_z.jpg'
-                        foto4='https://live.staticflickr.com/65535/51631364757_d306569ecc_z.jpg'
-                        foto5='https://live.staticflickr.com/65535/51631364792_ca80724900_z.jpg'
-                        foto6='https://live.staticflickr.com/65535/51631364842_d810512671_z.jpg' />
-                </div>
-                <Info
-                    teks='Teens Gekari'
-                    gambar="https://live.staticflickr.com/65535/51632853894_a65955c254_z.jpg" />
-                <hr />
+            <div>
+                <Container style={container} fluid>
+                    <div>
+                        <FotoFoto
+                            foto1='https://live.staticflickr.com/65535/51633040975_1455138117_z.jpg'
+                            foto2='https://live.staticflickr.com/65535/51631364697_a962098b27_z.jpg'
+                            foto3='https://live.staticflickr.com/65535/51632837819_e6810e9a95_z.jpg'
+                            foto4='https://live.staticflickr.com/65535/51631364757_d306569ecc_z.jpg'
+                            foto5='https://live.staticflickr.com/65535/51631364792_ca80724900_z.jpg'
+                            foto6='https://live.staticflickr.com/65535/51631364842_d810512671_z.jpg' />
+                    </div>
 
+                    <Info
+                        teks='Teens Gekari'
+                        gambar="https://live.staticflickr.com/65535/51632853894_a65955c254_z.jpg" />
+                </Container >
+                <br />
                 <InfoPKS
                     gambarPks='https://live.staticflickr.com/65535/51632853894_a65955c254_z.jpg'
                     teksNamaPks='Nama PKS'
@@ -33,7 +35,8 @@ class Taruna extends Component {
                     gambarPks='https://live.staticflickr.com/65535/51632853894_a65955c254_z.jpg'
                     teksNamaPks='Nama PKS'
                     teksKontakPks='Kontak PKS' />
-            </Container >
+            </div>
+
         )
     }
 }
