e616ced6c2f34aab65c6fe2aea900956aa5370c1
Sturgen <dodi.alexsander@gmail.com>
2021-10-27 13:26:14 +0700

    page taruna

--- src/pages/Taruna/index.js
+++ src/pages/Taruna/index.js
@@ -3,22 +3,29 @@ import { Container } from 'react-bootstrap';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import { FotoFoto, Info } from '../../components/besar';
+import { colors } from '../../random';
 import './taruna.css';
 
 class Taruna extends Component {
     render() {
         return (
-            <Container fluid>
-                <FotoFoto
-                    foto1='https://live.staticflickr.com/65535/51633040975_1455138117_z.jpg'
-                    foto2='https://live.staticflickr.com/65535/51631364697_a962098b27_z.jpg'
-                    foto3='https://live.staticflickr.com/65535/51632837819_e6810e9a95_z.jpg'
-                    foto4='https://live.staticflickr.com/65535/51631364757_d306569ecc_z.jpg'
-                    foto5='https://live.staticflickr.com/65535/51631364792_ca80724900_z.jpg'
-                    foto6='https://live.staticflickr.com/65535/51631364842_d810512671_z.jpg' />
+            <Container style={container} fluid>
+                <div>
+                    <FotoFoto
+                        foto1='https://live.staticflickr.com/65535/51633040975_1455138117_z.jpg'
+                        foto2='https://live.staticflickr.com/65535/51631364697_a962098b27_z.jpg'
+                        foto3='https://live.staticflickr.com/65535/51632837819_e6810e9a95_z.jpg'
+                        foto4='https://live.staticflickr.com/65535/51631364757_d306569ecc_z.jpg'
+                        foto5='https://live.staticflickr.com/65535/51631364792_ca80724900_z.jpg'
+                        foto6='https://live.staticflickr.com/65535/51631364842_d810512671_z.jpg' />
+                </div>
+                <Info />
             </Container >
         )
     }
 }
 
 export default Taruna;
+const container = {
+    backgroundColor: colors.background.grey
+}
